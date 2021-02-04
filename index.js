1bf0b2b99e0224282ed662ada3e14cae32f0f786
jomapormentilla <jomapormentilla@gmail.com>
2021-02-03 19:48:51 -0500

    commit tree 9a8bc36e68723a74d68b3f8cdc73df18ea2f5b26

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+function map( arr, func ) {
+    let newArr = []
+    for( let i=0; i<arr.length; i++ ){
+        newArr.push(func( arr[i] ))
+    }
+    return newArr
+}
+
+function reduce( arr, func, start ) {
+    let result
+    let i
+    if (!!start){
+        result = start
+        i = 0
+    } else {
+        result = arr[0]
+        i = 1
+    }
+
+    for( i; i<arr.length; i++ ){
+        result = func( arr[i], result )
+    }
+    return result
+}
